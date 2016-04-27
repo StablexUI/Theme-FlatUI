0d1769e0eb02d99315ddbc017359002432d52877
Alexander Kuzmenko <alex@stablex.ru>
2016-04-27 10:09:54 +0300

    Small and big text styles

--- src/sx/flatui/TextStyle.hx
+++ src/sx/flatui/TextStyle.hx
@@ -18,16 +18,33 @@ using sx.Sx;
  */
 class TextStyle
 {
-    static public inline var LIGHT   = 'lightText';
-    static public inline var ERROR   = 'errorText';
-    static public inline var SUCCESS = 'successText';
+    static public inline var LIGHT         = 'lightText';
+    static public inline var ERROR         = 'errorText';
+    static public inline var SUCCESS       = 'successText';
+    static public inline var SMALL         = 'defaultSmallText';
+    static public inline var LIGHT_SMALL   = 'lightSmallText';
+    static public inline var ERROR_SMALL   = 'errorSmallText';
+    static public inline var SUCCESS_SMALL = 'successSmallText';
+    static public inline var BIG           = 'defaultBigText';
+    static public inline var LIGHT_BIG     = 'lightBigText';
+    static public inline var ERROR_BIG     = 'errorBigText';
+    static public inline var SUCCESS_BIG   = 'successBigText';
+
 
     /** Which skins to use for each style */
     static private var __styles = [
-        Theme.DEFAULT_STYLE => {fontColor:FlatUITheme.FONT_COLOR_DARK, bold:false},
-        LIGHT               => {fontColor:FlatUITheme.FONT_COLOR_LIGHT, bold:false},
-        SUCCESS             => {fontColor:FlatUITheme.COLOR_EMERALD, bold:true},
-        ERROR               => {fontColor:FlatUITheme.COLOR_ALIZARIN, bold:true}
+        Theme.DEFAULT_STYLE => {fontColor:FlatUITheme.FONT_COLOR_DARK, bold:false, size:FlatUITheme.FONT_SIZE},
+        LIGHT               => {fontColor:FlatUITheme.FONT_COLOR_LIGHT, bold:false, size:FlatUITheme.FONT_SIZE},
+        SUCCESS             => {fontColor:FlatUITheme.COLOR_EMERALD, bold:true, size:FlatUITheme.FONT_SIZE},
+        ERROR               => {fontColor:FlatUITheme.COLOR_ALIZARIN, bold:true, size:FlatUITheme.FONT_SIZE},
+        SMALL               => {fontColor:FlatUITheme.FONT_COLOR_DARK, bold:false, size:FlatUITheme.FONT_SIZE_SMALL},
+        LIGHT_SMALL         => {fontColor:FlatUITheme.FONT_COLOR_LIGHT, bold:false, size:FlatUITheme.FONT_SIZE_SMALL},
+        SUCCESS_SMALL       => {fontColor:FlatUITheme.COLOR_EMERALD, bold:true, size:FlatUITheme.FONT_SIZE_SMALL},
+        ERROR_SMALL         => {fontColor:FlatUITheme.COLOR_ALIZARIN, bold:true, size:FlatUITheme.FONT_SIZE_SMALL},
+        BIG                 => {fontColor:FlatUITheme.FONT_COLOR_DARK, bold:false, size:FlatUITheme.FONT_SIZE_BIG},
+        LIGHT_BIG           => {fontColor:FlatUITheme.FONT_COLOR_LIGHT, bold:false, size:FlatUITheme.FONT_SIZE_BIG},
+        SUCCESS_BIG         => {fontColor:FlatUITheme.COLOR_EMERALD, bold:true, size:FlatUITheme.FONT_SIZE_BIG},
+        ERROR_BIG           => {fontColor:FlatUITheme.COLOR_ALIZARIN, bold:true, size:FlatUITheme.FONT_SIZE_BIG}
     ];
 
 
@@ -45,19 +62,19 @@ class TextStyle
     }
 
 
-    static public function template (widget:Widget, cfg:{fontColor:Int, bold:Bool}) : Void
+    static public function template (widget:Widget, cfg:{fontColor:Int, bold:Bool, size:Int}) : Void
     {
         var txt : Text = cast widget;
-        __setTextFormat(txt, cfg.fontColor, cfg.bold);
+        __setTextFormat(txt, cfg.fontColor, cfg.bold, cfg.size);
     }
 
 
     /**
      * Setup text formatting options
      */
-    static private inline function __setTextFormat (txt:Text, color:Int, bold:Bool) : Void
+    static private inline function __setTextFormat (txt:Text, color:Int, bold:Bool, size:Int) : Void
     {
-        var fontSize = FlatUITheme.FONT_SIZE.toPx();
+        var fontSize = size.toPx();
         var format   = FlatUITheme.textFormat(fontSize, color, bold);
         txt.setTextFormat(format);
     }
